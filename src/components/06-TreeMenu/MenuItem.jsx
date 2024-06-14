14a68e2e93a8fa1ea2f64312979243746f01224a
Armando Pi <armandopinedag@outlook.com>
2024-06-14 13:03:33 -0700

    implement toggle button to display tree nav menu

--- src/components/06-TreeMenu/MenuItem.jsx
+++ src/components/06-TreeMenu/MenuItem.jsx
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuList from "./MenuList";
 
 //2nd child - single item from menu
-function MenuItem({item}){
-  // console.log(item)
-  return(
+function MenuItem({ item }) {
+
+  //functionality to toggle on button click
+  const [displayCurrentChildren, setDisplayCurrentChildren] = useState({})
+
+  const itemHasChildren = item && item.children && item.children.length > 0;
+  // console.log('const chekea, bool: ', itemHasChildren);
+
+  function handleToggleChildrenBtn(currLabel) {
+    setDisplayCurrentChildren({ //a new obj
+      ...displayCurrentChildren, //keep old state values
+      [currLabel]: !displayCurrentChildren[currLabel], //add the 'label' passed as param
+      // AND asign true/false value depending if it exists ('label') in the obj
+    })
+  }
+  // console.log('displayCurrentChildren arroja: ', displayCurrentChildren);
+
+  return (
     <li>
-      <p>{item.label}</p>
+      <div style={{ display: "flex", alignItems: "center", gap: 18 }}>
+        <p>{item.label}</p>
+        { // + - buttons to click
+          itemHasChildren && <span onClick={() => handleToggleChildrenBtn(item.label)}>
+            {
+              displayCurrentChildren[item.label] ? "-" : "+"
+            }
+          </span>
+        }
+      </div>
       {
-        //check if current item has 'children'
-        item && item.children && item.children.length > 0 ? (
+        //check if current item has 'children' to display
+        itemHasChildren && displayCurrentChildren[item.label] ? (
           <MenuList list={item.children} />
         )
-        : null
+          : null
       }
     </li>
   )
--- src/components/06-TreeMenu/MenuList.jsx
+++ src/components/06-TreeMenu/MenuList.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import MenuItem from "./MenuItem";
 
 //1st child component, list of items
-function MenuList({list = []}) {
-  
-  return(
+function MenuList({ list = [] }) {
+
+  return (
     <ul className="uListContainer">
       {
-        list && list.length > 0 ? 
-        list.map(listItem => <MenuItem item={listItem} />)
-        : null
+        list && list.length > 0 ?
+          list.map(listItem => <MenuItem item={listItem} />)
+          : null
       }
     </ul>
   )
